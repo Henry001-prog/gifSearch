298ba7270147b11659791f9f9398c794c6b31ee2
Henry001-prog <alissonhenrique001@hotmail.com.br>
2020-09-18 15:32:00 -0300

    gif search application using the GIPHY API

---
+++ README.md
@@ -0,0 +1,3 @@
+# gif_search
+
+My fifth application created on Flutter during Udemy's Flutter course.
---
+++ android/app/src/main/kotlin/br/com/santana/buscador_gifs/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.santana.buscador_gifs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
